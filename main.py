5420a6e6ce70ee1bad08826fe67327085b922971
alexey <125185084+tsaritsennko@users.noreply.github.com>
2023-05-21 18:54:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ from aiogram.utils import executor
 bot = Bot(token='6023062210:AAFkUSbjhOy4V-4kRIIms6iYqM9KTgNivTA')
 dp = Dispatcher(bot)
 
-# Создаем словарь для хранения данных пользователей
-user_data = {}
 
-# Обрабатываем команду /start
-@dp.message_handler(commands=['start'])
+user_data = {} # Создаю словарь для хранения данных пользователей
+
+ 
+@dp.message_handler(commands=['start'])# Добавляем команду /start
 async def process_start_command(message: types.Message):
     await message.reply("Добро пожаловать! Как вас зовут?")
-    # Сохраняем ID пользователя в словарь
-    user_data[message.chat.id] = {}
+    
+    user_data[message.chat.id] = {}# Сохраняем ID пользователя в словарь
 
-# Обрабатываем ФИО
 @dp.message_handler(lambda message: message.chat.id in user_data and 'name' not in user_data[message.chat.id])
 async def process_name(message: types.Message):
     user_data[message.chat.id]['name'] = message.text
@@ -30,12 +29,12 @@ async def process_name(message: types.Message):
                                resize_keyboard=True
                            ))
 
-# Обрабатываем тип забега
+
 @dp.message_handler(lambda message: message.chat.id in user_data and 'race_type' not in user_data[message.chat.id]
                     and message.text in ["Полумарафон", "Марафон", "Сверхмарафон"])
 async def process_race_type(message: types.Message):
     user_data[message.chat.id]['race_type'] = message.text
-    # Сохраняем данные в файл excel
+    #Работаем с файлом excel,добавляя в ячейки ФИО и тип забега
     wb = openpyxl.Workbook()
     ws = wb.active
     ws['A1'] = "ФИО"
@@ -44,13 +43,13 @@ async def process_race_type(message: types.Message):
     ws.append(row)
     wb.save(filename='registration.xlsx')
 
-    # Удаляем кнопки после регистрации
-    reply_markup = types.ReplyKeyboardRemove()
+    
+    reply_markup = types.ReplyKeyboardRemove()# Удаляю кнопки после регистрации,чтобы они не оставались после регистрации
     await bot.send_message(chat_id=message.chat.id, text="Вы успешно зарегистрировались!",
                            reply_markup=reply_markup)
 
-    # Удаляем данные пользователя из словаря
-    del user_data[message.chat.id]
+    
+    del user_data[message.chat.id]# Удаляю данные пользователя из словаря
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
